2d2c0d8f666d482cec0393aef5d0ec4b49669f74
Johannes Raggam <thetetet@gmail.com>
2025-01-01 15:40:54 +0100

    Add some basic keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "1", '"+yy', { desc = "Copy to system clipboard", remap = true })
+map("n", "2", '"+p', { desc = "Paste before", remap = true })
+map("n", "@", '"+P', { desc = "Paste after", remap = true })
+map("n", "3", '"+d', { desc = "Delete into system clipboard", remap = true })
+
+-- Buffers
+-- -------
+map("n", "<C-x>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+
+-- Open buffer relative to current one
+--map("n", "<leader>e", "<cmd>n %:h/", { desc = "Open buffer relative to current one", remap = true })
