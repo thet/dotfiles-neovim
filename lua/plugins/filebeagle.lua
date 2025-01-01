3b6ad6f8a64956503cf462fe3143f8d0243b2038
Johannes Raggam <thetetet@gmail.com>
2025-01-01 15:35:47 +0100

    Add and configure vim-filebeagle.

---
+++ lua/plugins/filebeagle.lua
@@ -0,0 +1,24 @@
+-- https://github.com/jeetsukumaran/vim-filebeagle
+
+vim.cmd([[
+  "" filebeagle defines <leader>f, which I don't want
+  let g:filebeagle_suppress_keymaps = 1
+  let g:filebeagle_show_hidden = 0  " show with ``gh``
+  "let g:filebeagle_check_gitignore = 1
+]])
+
+return {
+  "jeetsukumaran/vim-filebeagle",
+  keys = {
+    {
+      "_",
+      "<Plug>FileBeagleOpenCurrentWorkingDir",
+      desc = "Browse working directory",
+    },
+    {
+      "-",
+      "<Plug>FileBeagleOpenCurrentBufferDir",
+      desc = "Browse buffer directory",
+    },
+  },
+}
