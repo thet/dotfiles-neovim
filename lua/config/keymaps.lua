650c312021f8e031a20f4baa7311b00a420fb4e5
Johannes Raggam <thetetet@gmail.com>
2025-10-20 23:03:00 +0200

    keymap: Migrate old nvim settings to lazyvim.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,10 +3,44 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-map("n", "1", '"+yy', { desc = "Copy to system clipboard", remap = true })
-map("n", "2", '"+p', { desc = "Paste before", remap = true })
-map("n", "@", '"+P', { desc = "Paste after", remap = true })
-map("n", "3", '"+d', { desc = "Delete into system clipboard", remap = true })
+-- Yank'in
+-- -------
+
+-- stylua: ignore start
+
+-- 🗑️ Delete key: send deletions to the black-hole register to not overwrite other yank registers.
+-- Thanks bairu from #vim!
+map({ "n", "v", "x" }, "<Del>", '"_x', { desc = "<Del> into blackhole.", noremap = true, silent = true })
+
+-- 📋 System clipboard: Copy, Paste, Delete
+-- Copy to system clipboard
+map('n', '1', '"+yy', { desc = "Copy line to system clipboard", noremap = true, silent = true })
+map('x', '1', '"+y',  { desc = "Copy selection to system clipboard", noremap = true, silent = true })
+
+-- Paste BEFORE (like 'P')
+map('n', '2', '"+p',  { desc = "Paste from system clipboard (after cursor)", noremap = true, silent = true })
+map('x', '2', '"_d"+P', { desc = "Paste from system clipboard (preserve +, before selection)", noremap = true, silent = true })
+
+-- Paste AFTER (like 'p')
+map('n', '@', '"+P',  { desc = "Paste from system clipboard (before cursor)", noremap = true, silent = true })
+map('x', '@', '"_d"+p', { desc = "Paste from system clipboard (preserve +, after selection)", noremap = true, silent = true })
+
+-- Delete into system clipboard
+map('n', '3', '"+dd', { desc = "Delete line into system clipboard", noremap = true, silent = true })
+map('x', '3', '"+d',  { desc = "Delete selection into system clipboard", noremap = true, silent = true })
+
+
+-- 🧷 Paste selected text into the command line
+-- In normal mode: yank the current line, start command-line, paste the yanked text
+map('n', ';', 'y:<C-r>"<C-b>', { desc = 'Paste last yanked text into command line', noremap = true, silent = true })
+-- In visual mode: yank selection, then open command line with that text
+map('x', ';', 'y:<C-r>"<C-b>', { desc = 'Paste visual selection into command line', noremap = true, silent = true })
+
+-- 📁 Copy filename/path to clipboard
+map('n', '<leader>cs', ':let @+=expand("%")<CR>', { desc = 'Copy relative file path to clipboard', noremap = true, silent = true })
+map('n', '<leader>cl', ':let @+=expand("%:p")<CR>', { desc = 'Copy absolute file path to clipboard', noremap = true, silent = true })
+
+-- stylua: ignore end
 
 -- Buffers
 -- -------
