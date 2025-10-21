1b83293633c2051f526b3dbff7b0eb46414d480b
Johannes Raggam <thetetet@gmail.com>
2025-10-22 00:41:16 +0200

    plugins: Set the pyright root to where nvim was started. Resp (the former not working yet) where pyright-root.txt file is present

---
+++ lua/plugins/pyright-root.lua
@@ -0,0 +1,19 @@
+-- Force pyright to use the nvim start directory as root_dir
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        root_markers = { "pyright-root.txt" },
+        --root_markers: { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", "pyrightconfig.json", ".git" }
+        -- -- ALWAYS use the cwd where you started nvim
+        -- root_dir = function(_)
+        --   -- use vim.loop.cwd(), not util.root_pattern, to force start-dir
+        --   local cwd = vim.loop.cwd()
+        --   vim.notify("[Pyright root_dir] Using: " .. cwd)
+        --   return cwd
+        -- end,
+      },
+    },
+  },
+}
