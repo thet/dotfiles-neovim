02a2ad1a4434a0576a791c220f6e22b77d601cf4
Johannes Raggam <thetetet@gmail.com>
2025-10-27 14:48:28 +0100

    Enforce foldmethod

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Force "indent" as foldmethod, even if TreeSitter or others have set it differently
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "typescriptreact", "json" },
+  callback = function()
+    vim.opt_local.foldmethod = "indent"
+    vim.opt_local.foldexpr = "0"
+  end,
+  desc = "Force indent folding for JS/TS files",
+})
