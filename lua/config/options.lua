5836d46ce7daa5230b753c98a2de566c9c807453
Johannes Raggam <thetetet@gmail.com>
2025-11-10 22:05:55 +0100

    config: Set mapleader to ","

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- set leader
+vim.g.mapleader = ","
+
 vim.opt.foldmethod = "indent"
