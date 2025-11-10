e35a4fa3509b7de18a80054e97f48caf1ae53916
Johannes Raggam <thetetet@gmail.com>
2025-11-10 21:47:31 +0100

    plugins: Add colorschemes.

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,55 @@
+-- More colorschemes:
+-- https://github.com/topics/neovim-colorscheme
+-- https://github.com/projekt0n/github-nvim-theme
+
+return {
+
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+    config = function()
+      vim.opt.background = "dark"
+    end,
+  },
+
+  -- Dracula
+  {
+    -- https://github.com/dracula/vim
+    -- https://draculatheme.com/vim
+    "dracula/vim",
+    lazy = true,
+    config = function()
+      vim.opt.background = "dark"
+    end,
+  },
+
+  -- GitHub Theme
+  {
+    -- https://github.com/endel/vim-github-colorscheme
+    "endel/vim-github-colorscheme",
+    lazy = true,
+    config = function()
+      vim.opt.background = "light"
+    end,
+  },
+
+  -- papercolor theme
+  {
+    -- https://github.com/NLKNguyen/papercolor-theme
+    "NLKNguyen/papercolor-theme",
+    lazy = true,
+    config = function()
+      vim.opt.background = "light"
+    end,
+  },
+
+  -- LazyVim theme configuration
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
