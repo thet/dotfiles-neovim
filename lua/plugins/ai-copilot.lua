5b934e5cafebb95284bf770dce62480ff88806ca
Johannes Raggam <thetetet@gmail.com>
2025-11-20 22:23:59 +0100

    ai: configure copilot-lsp for copilot.lua using mason to install the copilot lsp server - all that for copilot NES (next edit suggestion) support.

--- lua/plugins/ai-copilot.lua
+++ lua/plugins/ai-copilot.lua
@@ -3,6 +3,9 @@ return {
   -- https://github.com/zbirenbaum/copilot-cmp
   {
     "zbirenbaum/copilot.lua",
+    dependencies = {
+      "copilotlsp-nvim/copilot-lsp",
+    },
     cmd = "Copilot",
     build = ":Copilot auth",
     event = "InsertEnter",
@@ -18,6 +21,20 @@ return {
         markdown = true,
         help = true,
       },
+      -- copilot-lsp integration.
+      nes = {
+        enabled = true,
+        auto_trigger = false,
+      },
     },
+  },
+  -- https://github.com/neovim/nvim-lspconfig
+  -- https://github.com/copilotlsp-nvim/copilot-lsp?tab=readme-ov-file#usage
+  {
+    "copilotlsp-nvim/copilot-lsp",
+    init = function()
+      vim.g.copilot_nes_debounce = 500
+      vim.lsp.enable("copilot_ls")
+    end,
   },
 }
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+  -- https://github.com/mason-org/mason.nvim
+  -- https://github.com/mason-org/mason-registry
+  -- https://mason-registry.dev/registry/list?search=copilot
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "copilot-language-server",
+        "stylua",
+        "shfmt",
+      },
+    },
+  },
+}
