27e405c2e3ace11b328698d639874accd820cc74
Johannes Raggam <thetetet@gmail.com>
2025-11-28 16:44:18 +0100

    filebeagle: disable netrw completly in favor of filebeagle.

--- lua/plugins/filebeagle.lua
+++ lua/plugins/filebeagle.lua
@@ -1,5 +1,9 @@
 -- https://github.com/jeetsukumaran/vim-filebeagle
 
+-- Disable netrw. We use vim-filebeagle instead.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.cmd([[
   "" filebeagle defines <leader>f, which I don't want
   let g:filebeagle_suppress_keymaps = 1
