380343930274023f439998b7ba13450185e4f180
Johannes Raggam <thetetet@gmail.com>
2025-12-04 18:02:27 +0100

    Add git pluging junegunn/gv.

---
+++ lua/plugins/git-gv.lua
@@ -0,0 +1,7 @@
+return {
+  "junegunn/gv.vim", -- https://github.com/junegunn/gv.vim
+  event = "VeryLazy",
+  dependencies = {
+    "tpope/vim-fugitive", -- https://github.com/tpope/vim-fugitive
+  },
+}
