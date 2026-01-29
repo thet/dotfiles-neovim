a6fa36b602a43c6ac4210839b57f1c32b0fca748
Johannes Raggam <thetetet@gmail.com>
2026-01-29 14:41:45 +0100

    lsp-config: Disable biome lsp, didn't work yet. Try out later again!

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,47 +3,48 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = {
-        biome = {
+      --servers = {
+      --  biome = {
 
-          filetypes = { "css", "scss", "less", "javascript", "typescript", "json" },
-          settings = {
-            -- https://biomejs.dev/reference/configuration/
-            biome = {
-              formatter = {
-                -- https://biomejs.dev/reference/configuration/#formatteruseeditorconfig
-                useEditorConfig = true,
-              },
-            },
-          },
+      --    filetypes = { "css", "scss", "less", "javascript", "typescript", "json" },
+      --    settings = {
+      --      -- https://biomejs.dev/reference/configuration/
+      --      biome = {
+      --        formatter = {
+      --          -- https://biomejs.dev/reference/configuration/#formatteruseeditorconfig
+      --          useEditorConfig = true,
+      --        },
+      --      },
+      --    },
 
-          -- Get the root dir. Biome doesn't find it by itself. Necessary for respecting .editconfig.
-          root_dir = function(bufnr, on_dir)
-            -- 1. Get the full path of the current buffer
-            local startpath = vim.api.nvim_buf_get_name(bufnr)
-            if startpath == "" then
-              -- no path (e.g. [No Name] buffer) → do nothing, skip LSP
-              return
-            end
+      --    -- Get the root dir. Biome doesn't find it by itself. Necessary for respecting .editconfig.
+      --    root_dir = function(bufnr, on_dir)
+      --      -- 1. Get the full path of the current buffer
+      --      local startpath = vim.api.nvim_buf_get_name(bufnr)
+      --      if startpath == "" then
+      --        -- no path (e.g. [No Name] buffer) → do nothing, skip LSP
+      --        return
+      --      end
 
-            -- 2. Find the project root by looking upwards for common markers
-            local root = vim.fs.root(startpath, {
-              ".editconfig",
-              "biome.json",
-              "biome.jsonc",
-              "package.json",
-              ".git",
-            })
+      --      -- 2. Find the project root by looking upwards for common markers
+      --      local root = vim.fs.root(startpath, {
+      --        ".editorconfig",
+      --        "biome.json",
+      --        "biome.jsonc",
+      --        "package.json",
+      --        ".git",
+      --      })
 
-            -- 3. If we found a root, tell Neovim about it via on_dir
-            if root then
-              on_dir(root)
-              -- if you *only* want Biome with explicit config and not just any git repo,
-              -- you could require biome.json[?] and otherwise just `return` here.
-            end
-          end,
-        },
-      },
+      --      -- 3. If we found a root, tell Neovim about it via on_dir
+      --      if root then
+      --        vim.notify("Biome root: " .. root, vim.log.levels.INFO)
+      --        on_dir(root)
+      --      else
+      --        vim.notify("No Biome root found for: " .. startpath, vim.log.levels.WARN)
+      --      end
+      --    end,
+      --  },
+      --},
     },
   },
 }
