870dd8dd6781bc4d45bacae99e79320b7da60959
Johannes Raggam <thetetet@gmail.com>
2026-01-29 14:48:04 +0100

    files-dirbuf: Add plugin to edit a filesystem layout as if it was a buffer.

---
+++ lua/plugins/files-dirbuf.lua
@@ -0,0 +1,8 @@
+-- https://github.com/elihunter173/dirbuf.nvim
+-- https://www.reddit.com/r/neovim/comments/s5puxc/dirbufnvim_a_file_manager_which_lets_you_edit/
+
+return {
+  {
+    "elihunter173/dirbuf.nvim",
+  },
+}
