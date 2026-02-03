49a9b08969b575101811d370f29abe94b7e94d56
Johannes Raggam <thetetet@gmail.com>
2026-02-03 15:06:45 +0100

    disable venv-selector temporarily.

--- lua/plugins/python-venv-selector.lua
+++ lua/plugins/python-venv-selector.lua
@@ -1,3 +1,8 @@
+if true then
+  return {}
+end
+
+-- https://github.com/linux-cultist/venv-selector.nvim
 return {
   "linux-cultist/venv-selector.nvim",
   opts = { name = { ".venv", "venv", ".py3", "py3", ".py2", "py2" }, auto_refresh = true },
