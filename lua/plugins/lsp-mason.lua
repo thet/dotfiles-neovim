0f44b8738a727ba569eb89b86f574b0dda129924
Johannes Raggam <thetetet@gmail.com>
2026-03-08 11:31:26 +0100

    lsp-mason: Extend LSP server lists.

--- lua/plugins/lsp-mason.lua
+++ lua/plugins/lsp-mason.lua
@@ -8,11 +8,38 @@ return {
       ensure_installed = {
         "copilot-language-server",
         "biome",
-        "prettier",
-        "stylua",
-        "shfmt",
         "tailwindcss-language-server",
-        "ty",
+
+        -- Language servers for your languages
+        "ty", -- Python
+        "html-lsp", -- HTML
+        "css-lsp", -- CSS
+        "typescript-language-server", -- TypeScript/JavaScript
+        "lua-language-server", -- Lua
+        "rust-analyzer", -- Rust
+        "gopls", -- Go
+        "bash-language-server", -- Bash
+        "sqlls", -- SQL
+
+        -- Additional useful language servers
+        "json-lsp", -- JSON
+        "yaml-language-server", -- YAML
+        "dockerfile-language-server", -- Docker
+        "taplo", -- TOML
+        "marksman", -- Markdown
+
+        -- Linters and formatters
+        "prettier", -- Web development formatter
+        "stylua", -- Lua formatter
+        "shfmt", -- Shell script formatter
+        "eslint_d", -- JavaScript/TypeScript linter
+        "shellcheck", -- Shell script linter
+        "sqlfluff", -- SQL linter/formatter
+        "ruff", -- Python formatter and linter
+        "rustfmt", -- Rust formatter
+        "gofumpt", -- Go formatter
+        "golines", -- Go line length formatter
+        "goimports", -- Go import organizer
       },
     },
   },
