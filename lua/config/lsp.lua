42294e39c74b73e3e2d60f853eb7825ddaff4218
Johannes Raggam <thetetet@gmail.com>
2026-03-08 11:31:40 +0100

    lsp: More ty config.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,15 @@
 -- https://docs.astral.sh/ty/editors/#neovim
 -- https://github.com/mason-org/mason-registry/blob/2026-02-01-net-motion/packages/ty/package.yaml
 vim.lsp.enable("ty")
+
+vim.lsp.config("ty", {
+  settings = {
+    ty = {
+      configuration = {
+        rules = {
+          ["unresolved-reference"] = "warn",
+        },
+      },
+    },
+  },
+})
