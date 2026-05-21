b3b68a690687b055ad227ed5827e1a7a22829007
Johannes Raggam <thetetet@gmail.com>
2026-05-21 11:26:12 +0200

    ui-colorscheme.lua: darken diagnostic inline messages and set them italic.

--- lua/plugins/ui-colorscheme.lua
+++ lua/plugins/ui-colorscheme.lua
@@ -9,10 +9,16 @@ return {
     -- https://github.com/folke/tokyonight.nvim
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "moon" },
-    config = function()
-      vim.opt.background = "dark"
-    end,
+    opts = {
+      style = "moon",
+      on_highlights = function(hl, c)
+        local util = require("tokyonight.util")
+        hl.DiagnosticVirtualTextError = { fg = util.darken(c.error,   0.75), bg = util.darken(c.error,   0.15), italic = true }
+        hl.DiagnosticVirtualTextWarn  = { fg = util.darken(c.warning, 0.75), bg = util.darken(c.warning, 0.15), italic = true }
+        hl.DiagnosticVirtualTextInfo  = { fg = util.darken(c.info,    0.75), bg = util.darken(c.info,    0.15), italic = true }
+        hl.DiagnosticVirtualTextHint  = { fg = util.darken(c.hint,    0.75), bg = util.darken(c.hint,    0.15), italic = true }
+      end,
+    },
   },
 
   -- Dracula
