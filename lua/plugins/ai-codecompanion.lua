b643f12fe57406876aaeed499411b2541e8ef136
Johannes Raggam <thetetet@gmail.com>
2026-08-17 10:48:19 +0200

    ai-codecompanion.lua: Replicate the headless and isolated options for the chrome-devtools mcp server in the general mcp config. TODO: try out inherit_from_config again!

--- lua/plugins/ai-codecompanion.lua
+++ lua/plugins/ai-codecompanion.lua
@@ -276,6 +276,8 @@ return {
               "-y",
               "chrome-devtools-mcp@latest",
               "--executablePath=/usr/bin/chromium",
+              "--headless", -- Allow to run w/out X-Server
+              "--isolated", -- Allow to run multiple browser instances
             },
           },
         },
